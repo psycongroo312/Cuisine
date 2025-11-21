18444476b573a236681e6c191e92eaa91e3f2260
deni <deniphoto@mai.ru>
2025-11-21 17:45:20 +0300

    add header, logo and navmenu

---
+++ src/app/components/UI/Header.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { siteConfig } from "@/src/config/site.config";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Button,
+} from "@heroui/react";
+import { Image } from "@heroui/react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export const Logo = () => {
+  return (
+    <Image src="/food-logo.svg" alt={siteConfig.title} width={26} height={26} />
+  );
+};
+
+export default function Header() {
+  const pathname = usePathname();
+
+  const getNavItems = () => {
+    return siteConfig.navbarItems.map((item) => {
+      const isActive = pathname === item.href;
+      return (
+        <NavbarItem key={item.href}>
+          <Link
+            color="foreground"
+            href={item.href}
+            className={`px-3 py-1 | ${isActive ? "text-blue-500" : "text-foreground"}
+              hover:text-blue-300 hover:border
+              hover:border-blue-300 hover:rounded-md
+              transition-colors
+              transition-border
+              duration-200`}
+          >
+            {item.label}
+          </Link>
+        </NavbarItem>
+      );
+    });
+  };
+
+  return (
+    <Navbar>
+      <NavbarBrand>
+        <Link href="/" className="flex gap-1">
+          <Logo />
+          <p className="font-bold text-inherit">{siteConfig.title}</p>
+        </Link>
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        {getNavItems()}
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Логин</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Регистрация
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+}
---
+++ src/config/site.config.ts
@@ -0,0 +1,9 @@
+export const siteConfig = {
+  title: "Кухня",
+  description: "Рецепты кухни",
+  navbarItems: [
+    { href: "/", label: "Рецепты" },
+    { href: "/ingredients", label: "Ингредиенты" },
+    { href: "/about", label: "О нас" },
+  ],
+};
